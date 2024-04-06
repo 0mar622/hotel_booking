406074f80a8db2f549415d6920384509c7ce79ff
Omar Ahmed <omar@Omars-MacBook-Air-2.local>
2024-04-06 04:38:56 -0700

    updated cpp files

--- hotel_proj/hotel.h
+++ hotel_proj/hotel.h
@@ -2,7 +2,20 @@
 #define HOTEL_H_INCLUDED
 
 #include "room.h"
+#include <fstream>
+struct BookingRecord
+{
+    // string guestName;
+    int startDate;
+    int endDate;
+    int rNum;
+};
 
+struct GuestRecord
+{
+    struct BookingRecord bkr;
+    struct GuestRecord *next;
+};
 
 class Hotel
 {
@@ -46,16 +59,211 @@ public:
         r->setPrice(price);
     }
 
-    void setOccupy(int rmNum, int startDate, int endDate)
+    void addGuestRecord(int rmNum, int sd, int ed)
     {
         Room *r = getRoom(rmNum);
-        r->addGuestRecord(startDate, endDate);
+
+        FILE *file;
+
+        // Open the file in append mode
+        file = fopen("/Users/omar/git_projects/hotel_booking/hotel_proj/bookings.db", "a");
+
+        if (file == NULL) {
+            printf("Error opening file to store booking records\n");
+            return ;
+        }
+
+
+        GuestRecord *newBooking;
+        GuestRecord *nodePtr;
+
+        newBooking = new GuestRecord;
+        newBooking->bkr.startDate = sd;
+        newBooking->bkr.endDate = ed;
+        newBooking->bkr.rNum = rmNum;
+        newBooking->next = nullptr;
+
+        if(r->head == nullptr)
+        {
+            r->head = newBooking;
+        }
+        else
+        {
+            nodePtr = r->head;
+
+            while(nodePtr->next != nullptr)
+            {
+                nodePtr = nodePtr->next;
+            }
+
+            nodePtr->next = newBooking;
+        }
+
+        fwrite(&(newBooking->bkr), sizeof(newBooking->bkr), 1, file);
+
+        fclose(file);
+
+        printf("Booking record stored successfully!\n");
+
     }
 
-    void setFree(int rmNum, int startDate, int endDate)
+    void readGuestRecord()
+    {
+        // FILE IO IN PROGRESS
+
+        FILE *file;
+
+        // Open the file in append mode
+        file = fopen("/Users/omar/git_projects/hotel_booking/hotel_proj/bookings.db", "r");
+
+        if (file == NULL) {
+            printf("Error opening file to read booking records\n");
+            return ;
+        }
+
+
+        GuestRecord *newBooking;
+        GuestRecord *nodePtr;
+        struct BookingRecord bkr;
+        Room *r;
+
+        while(fread(&bkr, sizeof(bkr), 1, file) == 1)
+        {
+            newBooking = new GuestRecord;
+            newBooking->bkr.startDate = bkr.startDate;
+            newBooking->bkr.endDate = bkr.endDate;
+            newBooking->bkr.rNum = bkr.rNum;
+            newBooking->next = nullptr;
+            r = getRoom(bkr.rNum);
+
+            if(r->head == nullptr)
+            {
+                r->head = newBooking;
+            }
+            else
+            {
+                nodePtr = r->head;
+
+                while(nodePtr->next != nullptr)
+                {
+                    nodePtr = nodePtr->next;
+                }
+
+                nodePtr->next = newBooking;
+            }
+        }
+    }
+
+    void removeGuestRecord(int rmNum, int sd, int ed)
     {
         Room *r = getRoom(rmNum);
-        r->removeGuestRecord(startDate, endDate);
+
+        FILE *file;
+
+        // Open the file in read/write mode
+        file = fopen("/Users/omar/git_projects/hotel_booking/hotel_proj/bookings.db", "rw");
+
+        if (file == NULL) {
+            printf("Error opening file to store booking records\n");
+            return ;
+        }
+
+
+        GuestRecord *newBooking;
+        GuestRecord *nodePtr;
+        GuestRecord *prevBooking;
+        GuestRecord *nextBooking;
+        struct BookingRecord bkr;
+
+        // Reading from file to create Linked List before node deletion
+        while(fread(&bkr, sizeof(bkr), 1, file) == 1)
+        {
+            newBooking = new GuestRecord;
+            newBooking->bkr.startDate = bkr.startDate;
+            newBooking->bkr.endDate = bkr.endDate;
+            newBooking->bkr.rNum = bkr.rNum;
+            newBooking->next = nullptr;
+            r = getRoom(bkr.rNum);
+
+            if(r->head == nullptr)
+            {
+                r->head = newBooking;
+            }
+            else
+            {
+                nodePtr = r->head;
+
+                while(nodePtr->next != nullptr)
+                {
+                    nodePtr = nodePtr->next;
+                }
+
+                nodePtr->next = newBooking;
+            }
+        }
+
+        fseek(file, 0, SEEK_SET);
+
+        // Deletes node from Linked List
+        nodePtr = r->head;
+        prevBooking = r->head;
+        nextBooking = r->head;
+
+        if(r->head == nullptr)
+        {
+            cout << "There needs to be at least one booking in order to delete or cancel.\n";
+        }
+        else
+        {
+            nextBooking = nodePtr->next;
+            while(nodePtr != nullptr)
+            {
+                if((nodePtr->bkr.startDate == sd) && (nodePtr->bkr.endDate == ed))
+                {
+                    prevBooking->next = nextBooking;
+
+                    if(r->head == nodePtr)    // delete first booking
+                    {
+                        r->head = nextBooking;
+                    }
+                    delete nodePtr;
+                    break;
+                }
+
+                prevBooking = nodePtr;
+                nodePtr = nodePtr->next;
+                nextBooking = nodePtr->next;
+            }
+        }
+
+        nodePtr = r->head;
+        while(nodePtr != nullptr)
+        {
+            nodePtr->bkr.startDate = sd;
+            nodePtr->bkr.endDate = ed;
+            nodePtr->bkr.rNum = rmNum;
+            fwrite(&(nodePtr->bkr), sizeof(nodePtr->bkr), 1, file);
+            nodePtr = nodePtr->next;
+        }
+
+    }
+
+    void printAllRecords()
+    {
+        Room *r;
+        GuestRecord *tmp;
+
+        for(int i = 0; i < total_rooms; i++)
+        {
+            r = &room_list[i];
+            tmp = r->head;
+            while(tmp != nullptr)
+            {
+                cout << "Room " << tmp->bkr.rNum << " booked for dates " << tmp->bkr.startDate << " and " << tmp->bkr.endDate << endl;
+                tmp = tmp->next;
+            }
+        }
+
     }
 
     int getRoomCount()
--- hotel_proj/main.cpp
+++ hotel_proj/main.cpp
@@ -13,9 +13,9 @@ int main()
     htl->setRoomProps(102, "triple", 109.99);
     htl->setRoomProps(103, "single", 89.99);
     htl->setRoomProps(104, "single", 79.99);
-    htl->setRoomProps(105, "triple", 119.99);
+   // htl->setRoomProps(105, "triple", 119.99);
     htl->setRoomProps(106, "double", 104.99);
-    htl->setRoomProps(107, "triple", 119.99);
+   // htl->setRoomProps(107, "triple", 119.99);
     htl->setRoomProps(108, "double", 99.99);
     htl->setRoomProps(109, "double", 104.99);
     htl->setRoomProps(110, "single", 99.99);
@@ -30,55 +30,15 @@ int main()
 
 
     Room *r;
+    htl->readGuestRecord();
 
     int total = htl->getRoomCount();
     cout << total << " rooms in the hotel\n";
 
-    char tokens[10000];
-    char rNum[3];
-
-    #if 0
-    ifstream bookedDates;
-    bookedDates.open("/Users/omar/git_projects/hotel_booking/hotel_proj/bookings_record.txt");
-    if(!bookedDates.is_open())
-    {
-        cout << "ERROR ACCESSING bookedDates\n";
-        return 1;
-    }
-
-    int i = 0;
-    while(getline(bookedDates, tokens[i]))
-    {
-        if(tokens[i] == ' ')
-        {
-
-            if((int(tokens[i+1]) >= 48) && (int(tokens[i+1]) <= 57))
-            {
-                if((int(tokens[i+2]) >= 48) && (int(tokens[i+2]) <= 57))
-                {
-                    if((int(tokens[i+3]) >= 48) && (int(tokens[i+3]) <= 57))
-                    {
-                        int k = 0;
-                        for(int j = i+1; j < i+3; j++)
-                        {
-                            rNum[k] = tokens[j];
-                        }
-                    }
-                }
-            }
-
-
-        }
-    }
-    #endif
-
-
-
-   // int currDate = rand() % 31 + 1;
 
     while(true)
     {
-      //  cout << "Today is " << currDate << endl;
+       // htl->readGuestRecord();
 
         cout << "Pick a room style (single, double, triple): ";
         cin >> style;
@@ -89,37 +49,15 @@ int main()
         cout << "Enter the date to check out: ";
         cin >> endDate;
 
+
         r = htl->findAvailableRoom(style, startDate, endDate);
 
         if(r != nullptr)
         {
-            htl->setOccupy(r->getRoomNum(), startDate, endDate);
+            htl->addGuestRecord(r->getRoomNum(), startDate, endDate);
             cout << "You got Room " << r->getRoomNum() << endl;
             num_of_nights = endDate - startDate;
             cout << "Total Cost of stay: $" << num_of_nights * r->getPrice() << endl;
-
-            // Room Keys
-           /* if(currDate <= startDate)
-            {
-                cout << "How many room keys do you want for your room? ";
-                cin >> num_keys;
-                RoomKey *key = new RoomKey[num_keys];
-
-                if(currDate == startDate)   // Only to be executed once
-                {
-                    for(int i = 0; i < num_keys; i++)
-                    {
-                        key->setActivity(true);
-                        key++;
-                    }
-                }
-            }
-
-            else
-            {
-                cout << "Date " << startDate << " already past\n";
-            }
-                */
             cout << "\n";
         }
 
@@ -130,20 +68,22 @@ int main()
 
 
         // Options to Cancel or View bookings, or just keep going
-        cout << "Do you want to cancel booking? (1. Yes, 2. No, 3. View Bookings)\n";
+        cout << "Do you want to cancel booking? (1. Yes, 2. No, 3. View Bookings, 4. Exit Program)\n";
         cin >> cancel_num;
 
         if(cancel_num == 1)
         {
-            cout << "All the bookings for Room " << r->getRoomNum() << ":\n";
-            r->printAllRecords();
+            cout << "All the bookings:\n";
+            htl->printAllRecords();
             cout << "Provide start date for cancellation: ";
             cin >> startDate;
 
             cout << "Provide end date for cancellation: ";
             cin >> endDate;
 
-            htl->setFree(r->getRoomNum(), startDate, endDate);
+
+
+            htl->removeGuestRecord(r->getRoomNum(), startDate, endDate);
 
             cout << "Booking of dates " << startDate << " to " << endDate << " for Room " << r->getRoomNum() << " cancelled.\n";
         }
@@ -155,8 +95,13 @@ int main()
 
         else if(cancel_num == 3)
         {
-            cout << "All the bookings for Room " << r->getRoomNum() << ":\n";
-            r->printAllRecords();
+            cout << "All the bookings:\n";
+            htl->printAllRecords();
+        }
+
+        else if(cancel_num == 4)
+        {
+            break;
         }
 
         else
